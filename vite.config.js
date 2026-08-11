a24c7e906804d919c73ef861da2cf9a76783fa14
spooky <spookyeats4skin@gmail.com>
2026-08-11 23:01:18 +0530

    clean old api calls

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      },
-      '/uploads': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
-  },
   build: {
     rollupOptions: {
       output: {
