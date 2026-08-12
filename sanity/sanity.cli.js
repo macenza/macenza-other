8b9e05f611f18acaa72db46e3b4bc545ab5cfa39
spooky <spookyeats4skin@gmail.com>
2026-08-12 14:01:24 +0530

    add sanity appid

--- sanity/sanity.cli.js
+++ sanity/sanity.cli.js
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'nc73q0nf1q4iho3ikkikgt4o',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
